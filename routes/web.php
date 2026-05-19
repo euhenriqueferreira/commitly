f7d554c9540bdfd38034bc0ab4ea32c4435ecd4e
Quinho Pereira <henriqueferreira0320@gmail.com>
2026-05-18 21:06:09 -0300

    authentication routes

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/login', 'pages::authentication.login');
+Route::prefix('/auth')->group(function(){
+    Route::livewire('/cadastro', 'pages::authentication.register');
+    Route::livewire('/login', 'pages::authentication.login');
+    Route::livewire('/verificacao', 'pages::authentication.otp-verification');
+    Route::livewire('/sucesso', 'pages::authentication.otp-success');
+    Route::livewire('/codigos-de-recuperacao', 'pages::authentication.recovery-codes');
+    Route::livewire('/recuperacao', 'pages::authentication.recovery');
+    Route::livewire('/redefinicao', 'pages::authentication.email-reset');
+});
